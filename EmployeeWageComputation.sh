d7622e75a2967dc534f053d2cda09b2e6e355f3a
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-07 13:21:02 +0530

    ADDED, Calculate Work For 100 Hours

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -8,10 +8,11 @@ HALFDAY_WORKING_HR=4
 WORK_PER_MONTH=20
 absentEmp=0
 day=0
+hours=0
 fullTimeEmp=1
 partTimeEmp=2
 
-	while (( $day < $WORK_PER_MONTH ))
+	while (( $day < $WORK_PER_MONTH && $hours < 100 ))
 	do
 
 	empCheck=$((RANDOM%2))
@@ -22,10 +23,12 @@ partTimeEmp=2
 				echo "Employee Absent";;
 		$fullTimeEmp)
 				echo "FullTime Employee Present"
-				echo $(($WAGE_PER_HR * $FULLDAY_WORKING_HR ));;
+				echo $(($WAGE_PER_HR * $FULLDAY_WORKING_HR ))
+				hours=$(($hours + $FULLDAY_WORKING_HR));;
 		$partTimeEmp)
 				echo "PartTime Employee Present"
-				echo $(($WAGE_PER_HR * $HALFDAY_WORKING_HR ));;
+				echo $(($WAGE_PER_HR * $HALFDAY_WORKING_HR ))
+				hours=$(($hours + $HALFDAY_WORKING_HR));;
 			*)
 				echo "Invalid Option";;
 	esac
