41d4c57b00ecfbb9de66470b6aa63cec53fcecad
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-07 12:49:51 +0530

    ADDED, Calculate Wage For Month

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -5,17 +5,29 @@ echo "Welcom to Employee Wage Computation program"
 WAGE_PER_HR=20
 FULLDAY_WORKING_HR=8
 HALFDAY_WORKING_HR=4
+WORK_PER_MONTH=20
 absentEmp=0
+day=0
 fullTimeEmp=1
 partTimeEmp=2
 
+	while (( $day < $WORK_PER_MONTH ))
+	do
+
 	empCheck=$((RANDOM%2))
 
-	if (( $empCheck == 0 ))
-	then
-		echo "Employee Absent"
-	else
-		echo "Employee Present"
-		echo $(($WAGE_PER_HR * $WORKING_HR )) 
-	fi
+	case $empCheck in
 
+		absentEmp)
+				echo "Employee Absent";;
+		$fullTimeEmp)
+				echo "FullTime Employee Present"
+				echo $(($WAGE_PER_HR * $FULLDAY_WORKING_HR ));;
+		$partTimeEmp)
+				echo "PartTime Employee Present"
+				echo $(($WAGE_PER_HR * $HALFDAY_WORKING_HR ));;
+			*)
+				echo "Invalid Option";;
+	esac
+	day=$(($day+1))
+done
