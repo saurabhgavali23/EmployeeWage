45baeb438621d83a49fafc241e1c6e5b8316316e
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-07 12:17:53 +0530

    MODIFY, Varibale name

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -6,9 +6,9 @@ WAGE_PER_HR=20
 FULLDAY_WORKING_HR=8
 HALFDAY_WORKING_HR=4
 
-	rand=$((RANDOM%2))
+	empCheck=$((RANDOM%2))
 
-	if (( $rand == 0 ))
+	if (( $empCheck == 0 ))
 	then
 		echo "Employee Absent"
 	else
