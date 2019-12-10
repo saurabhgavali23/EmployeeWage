f2e1e43afe36a8485b25a76e814551d95fe7e4f5
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-10 18:19:22 +0530

    REFACTOR: Coding Functionalities

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -3,59 +3,45 @@
 echo "Welcom to Employee Wage Computation program"
 
 WAGE_PER_HR=20
-FULLDAY_WORKING_HR=8
-HALFDAY_WORKING_HR=4
 WORK_PER_MONTH=20
-absentEmp=0
+FULL_TIME_EMP=1
+PART_TIME_EMP=2
+
 day=0
-totalDay=0
-hours=0
+totalHours=0
 monthlyWage=0
-dayHour=0
-fullTimeEmp=1
-partTimeEmp=2
-count=0
 
 declare -A storeTotalDailyWage
 
 function getWorkHrs(){
 	local empCheck=$1
 
-	if (( $empCheck == 1 ))
-	then
-		echo	$(($hour + $FULLDAY_WORKING_HR))
-	else
-		echo	$(($hour + $HALFDAY_WORKING_HR))
-	fi
-}
-
-	while (( $day < $WORK_PER_MONTH && $hours < 100 ))
-	do
-
-	empCheck=$((RANDOM%3))
-
 	case $empCheck in
 
-		$absentEmp)
-				echo "Employee Absent";;
-		$fullTimeEmp)
-				echo "FullTime Employee Present"
-				dailyWage=$(( $day + $FULLDAY_WORKING_HR ))
-				monthlyWage=$(( $dailyWage * $WAGE_PER_HR ))
-				hours="$( getWorkHrs $fullTimeEmp )"
-				storeTotalDailyWage["Day_$day"]="$dailyWage	$monthlyWage";;
-		$partTimeEmp)
-				echo "PartTime Employee Present"
-				dailyWage=$(( $day + $HALFDAY_WORKING_HR ))
-				monthlyWage=$(( $dailyWage * $WAGE_PER_HR ))
-				hours="$( getWorkHrs $partTimeEmp )"
-				storeTotalDailyWage["Day_$day"]="$dailyWage	$monthlyWage";;
+	$FULL_TIME_EMP)
+			echo 8;;
+
+	$HALF_TIME_EMP)
+			echo 4;;
+
+	*)
+			echo 0;;
 	esac
+}
+
+while (( $day < $WORK_PER_MONTH && $totalHours < 100 ))
+do
+
+	empCheck=$((RANDOM%3))
+	empHrs=$( getWorkHrs $empCheck )
+	dailyWage=$(($empHrs + $WAGE_PER_HR ))
+	monthlyWage=$(( $dailyWage * $WORK_PER_MONTH ))
+	totalHours=$(( $totalHours + $empHrs ))
+	storeTotalDailyWage["Day_$day"]="$dailyWage	$monthlyWage"
 	day=$(($day+1))
 done
 
-len=${#storeTotalDailyWage[@]}
-for (( i=0; i<$len;i++ ))
+for (( i=1; i<${#storeTotalDailyWage[@]};i++ ))
 do
 		echo "Day_$i		${storeTotalDailyWage[Day_$i]}"
 done
